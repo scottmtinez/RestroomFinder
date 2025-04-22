193af549f7c7293d83197a0f898df8b28163a7a9
Scott Martínez <scottmtinez@gmail.com>
2025-04-22 09:02:42 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
     return (
         <div className="Home-container">
             <div className='Home-image'>
-                
+                *image*
             </div>
             
-            <div className=''>
+            <div className='Home-'>
                 
             </div>
         </div>
