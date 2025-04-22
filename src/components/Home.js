5c3cb2a4c819b48f6da8b8bc85079a74541ddaa7
Scott Martínez <scottmtinez@gmail.com>
2025-04-22 08:32:13 -0500

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="Home-container">
+            <div className='Home-image'>
+                
+            </div>
+            
+            <div className=''>
+                
+            </div>
+        </div>
+
+    );
+}
+
+export default Home;
