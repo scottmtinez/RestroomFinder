37cba11207687331cd9cfabd3b2c165e0fa20d74
Scott Martínez <scottmtinez@gmail.com>
2025-04-22 08:28:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-
- 
+      <Nav />
+      
     </div>
   );
 }
