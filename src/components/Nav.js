119f713abbf1751bd73163824a9c291435b403c1
Scott Martínez <scottmtinez@gmail.com>
2025-04-23 19:19:23 -0500

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import Home from './Home';
-import './Nav.css';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import FindRestroom from './components/FindRestroom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Account from './components/Account';
 
-function Nav() {
-    return (
-        <div className="Nav-container">
-            <nav className='Nav-bar'>
-                <ul className='Nav-list'>
-                    <li className=''><h1 className='Nav-title'>Restroom Finder</h1></li>
-                    <li className='Nav-item'>Home</li>
-                    <li className='Nav-item'>Find Restroom</li>
-                    <li className='Nav-item'>About Us</li>
-                    <li className='Nav-item'>Contact Us</li>
-                    <li className='Nav-item'>Account</li>
-                </ul>
-            </nav>
-
-            <Home />
-
-        </div>
-
-    );
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/find" element={<FindRestroom />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </div>
+      <footer className='Nav-footer'><p className='Nav-footer-text'>© 2023 Restroom Finder. All rights reserved.</p></footer>
+    </Router>
+  );
 }
 
-export default Nav;
+export default App;
