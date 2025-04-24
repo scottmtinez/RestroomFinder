a4b20d84c8893274dbc1f1a58a6a2b46befb172c
Scott Martínez <scottmtinez@gmail.com>
2025-04-23 19:11:47 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,37 +4,23 @@ import './Home.css';
 function Home() {
     return (
         <div className="Home-container">
-            <div className='Home-image'>
-                *image*
-            </div>
-            
-            <div className='Home-packages'>
-                <div className='Home-packages-item'>
-
-                </div>
-
-                <div className='Home-packages-item'>
-
-                </div>
-
-                <div className='Home-packages-item'>
-
-                </div>
-
-                <div className='Home-packages-item'>
-
-                </div>
-
-                <div className='Home-packages-item'>
-
-                </div>
+            <div className="Home-image">
+                <img src="/assets/banner1.jpg" alt="Restroom Finder Banner" />
             </div>
 
-            <div className='Home-image'>
-                *image*
+            <div className="Home-packages">
+                {[...Array(5)].map((_, i) => (
+                    <div className="Home-packages-item" key={i}>
+                        <h3>Feature {i + 1}</h3>
+                        <p>Short description of this feature.</p>
+                    </div>
+                ))}
+            </div>
+
+            <div className="Home-image">
+                <img src="/assets/banner2.jpg" alt="Another Banner" />
             </div>
         </div>
-
     );
 }
 
